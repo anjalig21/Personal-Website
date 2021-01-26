154b7e9d384460960c964af7ef78e51fbebc4bbe
Anjali Gupta <anjaligta@outook.com>
2021-01-26 12:33:52 -0500

    added styling for navigation bar

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React from 'react';
+import {FaGithub, FaLinkedin, FaMailBulk} from "react-icons/fa";
+
 
 const banner = () => {
     return (
         <header className = "header">
+            <div className = "container">
+                <div className = "row">
+                    <div className="col-1">
+                        <div className="header__content">
+                            <div className="header__section">
+                                <ul className="header__ul">
+                                    <li>
+                                        <FaGithub />
+                                    </li>
+                                    <li>
+                                        <FaLinkedin />
+                                    </li>
+                                    <li>
+                                        <FaMailBulk />
+                                    </li>
+                                </ul>
+                                <p> Hello! My name is, </p>
+                                <h1> Anjali Gupta </h1>
+                                <p> And I love to code!</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-2">
+
+                    </div>
+                </div>
+            </div>
             
         </header>
     )
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,22 +1,27 @@
-import React from 'react'
+import React from 'react';
+
+// File Imports
+import my_logo from '../images/anjali.png';
 
 const navbar = () => {
     return (
         <navbar className = "navbar">
             <div className = "container">
-                <ul className = "navbar__left">
-                    <div className = "navbar__left.logo">
-                        <img src="../images/anjali.png" alt="logo" />
-                    </div>
-                </ul>
-                <ul className = "navbar__right">
-                    <li> <a href = "" >Home</a></li>
-                    <li> <a href = "" >About</a></li>
-                    <li> <a href = "" >Skills</a></li>
-                    <li> <a href = "" >Projects</a></li>
-                    <li> <a href = "" >Blog</a></li>
-                    <li> <a href = "" >Contact</a></li>
-                </ul>
+                <div className = "navbar__container">
+                    <ul className = "navbar__left">
+                        <div className = "navbar__left-logo">
+                            <img src= {my_logo} alt="logo" />
+                        </div>
+                    </ul>
+                    <ul className = "navbar__right">
+                        <li> <a href = "" >Home</a></li>
+                        <li> <a href = "" >About</a></li>
+                        <li> <a href = "" >Skills</a></li>
+                        <li> <a href = "" >Projects</a></li>
+                        <li> <a href = "" >Blog</a></li>
+                        <li> <a href = "" >Contact</a></li>
+                    </ul>
+                </div>
             </div>
         </navbar>
     )
