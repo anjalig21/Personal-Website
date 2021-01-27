9917961d544c8bb88d66afe7f987f66211926432
Anjali Gupta <anjaligta@outook.com>
2021-01-26 22:34:58 -0500

    updates positioning of moving background

---
+++ src/components/skills.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Skills = () => {
+    return (
+        <div className = "skills">
+            <div className = "container">
+                <div className = "skills__header">
+                    <h3 className = "heading">SKILLS</h3>
+                    <h1 className = "mainHeader">My Skills</h1>
+                    <p className = "mainContent">lorem ipsum eoif jei fje fjef j</p>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Skills
