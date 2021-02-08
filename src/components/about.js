44e92101eae7786424ae37897626453a8c20b70f
anjalig21 <anjaligta@outlook.com>
2021-02-08 01:39:05 -0500

    updated About component

--- src/components/about.js
+++ src/components/about.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import '../CSS/about.css';
 import { Grid } from '@material-ui/core';
+import ImageContentHover from 'react-image-hover';
+import photoMe from '../images/me_again.png';
 
 const About = () => {
     return (
-        <Grid className="about" container direction={'row'} spacing={1}>
+        <Grid className="about" container direction={'row'}>
             <Grid item xs={12}>
                 &nbsp;
                 <h1 className="mainHeaderProjects">About Me</h1>
                 <p className="mainContentProjects">- Get To Know Me! -</p>
                 <div className="commonBorderProjects"></div>
                 &nbsp;
+            </Grid>
+            <Grid className="photoMe" item xs={5}>
+                <ImageContentHover
+                    tileWidth="350px"
+                    tileHeight="350px"
+                    image={photoMe}
+                    content={{
+                        title: 'Image Title',
+                        body: 'Image content viewer'
+                    }}
+                    effect="fadeIn" />
             </Grid>
         </Grid>
     )
