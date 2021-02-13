31e56f8324eccaac25ad054bcbeed18ab74653c7
anjalig21 <anjaligta@outlook.com>
2021-02-13 18:39:20 -0500

    updated image in banner

--- src/components/banner.js
+++ src/components/banner.js
@@ -9,63 +9,60 @@ import my_photo from '../images/me.png';
 
 const Banner = () => {
     const [state] = React.useState({
-        title: "Anjali Gupta", text1: "Hello! My name is,", text2: "And I am a"})
+        title: "Anjali Gupta", text1: "Hello! My name is,", text2: "And I am a"
+    })
 
     return (
         <header className="header">
-            <div className="container">
-                <div className="row">
-                    <div className="col-1">
-                        <div className="header__content">
-                            <div className="header__section">
-                                <ul className="header__ul">
-                                    <li className = "Button">
-                                        <FaGithub />
-                                    </li>
-                                    <li className = "Button">
-                                        <FaLinkedin />
-                                    </li>
-                                    <li className = "Button">
-                                        <BsFillPersonFill />
-                                    </li>
-                                    <li className = "Button">
-                                        <FaLightbulb />
-                                    </li>
-                                    <li className = "Button">
-                                        <FaBlog />
-                                    </li>
-                                </ul>
-                                <p className="para1"> {state.text1} </p>
-                                <h1 className="head1"> {state.title} </h1>
-                                <div className="information">
-                                    <p className="para2"> {state.text2}&nbsp;</p>
-                                    <div className="typewriter">
-                                        <Typewriter
-                                            options={{
-                                                strings: ['Developer', 'Technology Enthusiast', 'Designer'],
-                                                autoStart: true,
-                                                loop: true,
-                                            }}
-                                        />
-                                    </div>
-                                </div>
-                                <div className="header__buttons">
-                                    <a href="" className="btn btn-portfolio">
-                                        My Portfolio
-                                    </a>
-                                    &nbsp;&nbsp;&nbsp;
-                                    <a href="" className="btn btn-contact">
-                                        Contact Me
-                                    </a>
-                                </div>
+            <div className="col">
+                <div className="header__content">
+                    <div className="header__section">
+                        <ul className="header__ul">
+                            <li className="Button">
+                                <FaGithub />
+                            </li>
+                            <li className="Button">
+                                <FaLinkedin />
+                            </li>
+                            <li className="Button">
+                                <BsFillPersonFill />
+                            </li>
+                            <li className="Button">
+                                <FaLightbulb />
+                            </li>
+                            <li className="Button">
+                                <FaBlog />
+                            </li>
+                        </ul>
+                        <p className="para1"> {state.text1} </p>
+                        <h1 className="head1"> {state.title} </h1>
+                        <div className="information">
+                            <p className="para2"> {state.text2}&nbsp;</p>
+                            <div className="typewriter">
+                                <Typewriter
+                                    options={{
+                                        strings: ['Developer', 'Technology Enthusiast', 'Designer'],
+                                        autoStart: true,
+                                        loop: true,
+                                    }}
+                                />
                             </div>
                         </div>
-                    </div>
-                    <div className="col-1">
-                        <div className="banner__img">
-                            <img src={my_photo} alt="photo of me" />
+                        <div className="header__buttons">
+                            <a href="" className="btn btn-portfolio">
+                                My Portfolio
+                            </a>
+                            &nbsp;&nbsp;&nbsp;
+                            <a href="" className="btn btn-contact">
+                                Contact Me
+                            </a>
                         </div>
                     </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className="banner__img">
+                    <img src={my_photo} alt="photo of me" />
                 </div>
             </div>
         </header>
--- src/components/contact.js
+++ src/components/contact.js
@@ -37,30 +37,8 @@ const Contact = () => {
                 <p className="mainContentProjectsContact">- Let's Connect! -</p>
                 &nbsp;
             </Grid>
-            <Grid item xs={6}>
-                <h2 className="contact-heading"> Get in Touch</h2>
-                <h4 className="contact-heading2"> I am currently seeking web developement and
-                software engineering internship/co-op opportunities for Fall 2021.
-                Feel free to contact me whenever with the form on the right or my Linkedin! </h4>
-                <br></br>
-                <h2 className="contact-heading"> Check Out My Work</h2>
-                <ul className="header_list">
-                    <li className="Button">
-                        <FaGithub />
-                    </li>
-                    <br/>
-                    <li className="Button">
-                        <FaLinkedin />
-
-                    </li>
-                    <br/>
-                    <li className="Button">
-                        <AiFillFileWord/>
-                    </li>
-                </ul>
-            </Grid>
-            <Grid item xs={1}></Grid>
-            <Grid item xs={5}>
+            <Grid item xs={3}></Grid>
+            <Grid item xs={7}>
                 <form className="form" onSubmit={submitHander}>
                     <label>Name</label>
                     <br />
